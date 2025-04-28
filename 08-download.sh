654156b802c0a91b50c9f5dfd629157dc2ff6aa5
Justin Waltrip <justin@cognistx.com>
2025-04-28 13:30:39 +0000

    Add download and upload scripts for S3 file management

---
+++ 08-download.sh
@@ -0,0 +1,2 @@
+# download s3://sqr-bucket-194-prod/assets/exports/project_royal.zip to current directory
+aws s3 cp s3://sqr-bucket-194-prod/assets/exports/project_royal.zip .
---
+++ 09-upload.sh
@@ -0,0 +1,2 @@
+# upload project_royal.zip to s3://crown-royal/project_royal.zip
+aws s3 cp project_royal.zip s3://crown-royal/project_royal.zip
