57123b75ac41a02ad1e433a6f88239b8f80f3ed3
Justin Waltrip <justin@cognistx.com>
2025-05-14 16:26:28 +0000

    Refactor Python environment setup in devenv.nix; add requirements.txt for package management

--- devenv.nix
+++ devenv.nix
@@ -10,23 +10,13 @@
   ];
   languages.python = {
     enable = true;
-    venv = {
-      enable = true;
-      requirements = ''
-        boto3
-        tqdm
-        pywin32
-        Pillow
-        PyPDF2
-        tabulate 
-        matplotlib 
-        seaborn
-      '';
-    };
-    version = "3.11"; # Explicitly specify Python 3.11
+    venv.enable = true;
+    version = "3.11"; # Required for awscli2
+    uv.enable = true;
   };
   dotenv.enable = true;
   enterShell = ''
+    uv pip install -r requirements.txt
     clear
   '';
   scripts.clean.exec = "rm -rf downloads extracted_files supported_files pdf_page_counts_supported_files.json";
